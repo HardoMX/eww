30b146a736f49886d10c986379b8c874055bb9dd
Hardo MX <hardo314@gmail.com>
2023-08-02 10:50:29 +0200

    Update css and weather

--- dashboard/scripts/weather.sh
+++ dashboard/scripts/weather.sh
@@ -6,15 +6,15 @@ weather=`curl -sf "http://api.openweathermap.org/data/2.5/weather?APPID="$KEY"&i
 
 if [[ "$1" == "--temp" ]]; then
     echo $weather | jq -r ".main.temp"
-elif [[ "$1" == "--icon" ]]; then
-    echo "./images/icons/weather.png"
 elif [[ "$1" == "--feels" ]]; then
     echo $weather | jq -r ".main.feels_like"
 elif [[ "$1" == "--condition" ]]; then
     echo $weather | jq -r ".weather[].description"
 elif [[ "$1" == "--locale" ]]; then
-    echo $weather | jq ".name"
-    echo $weather | jq ".sys.country"
+    echo $weather | jq -r ".name"
+    echo $weather | jq -r ".sys.country"
+elif [[ "$1" == "--icon" ]]; then
+    echo $weather | jq -r ".weather[].icon" | head -1
 else
     echo "bad syntax, needs --<command>"
 fi
